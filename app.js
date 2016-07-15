37f60fbaa7dd1c0f6a372a894bd7e61646920bf9
Vincent Hofmeister <vincentallanhofmeister@gmail.com>
2016-07-15 19:12:43 -0400

    add favicon

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'handlebars');
 //end handlebars setup
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
