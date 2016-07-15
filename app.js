781faee33b34baf5e79ece5c452e9f868409e552
Vincent Hofmeister <vincentallanhofmeister@gmail.com>
2016-07-15 18:57:46 -0400

    add express-handlebars module, add handlebars config to app.js

--- app.js
+++ app.js
@@ -10,9 +10,11 @@ var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// view engine/handlebars setup setup
+var handlebars = require('express-handlebars').create({defaultLayout: 'default'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+//end handlebars setup
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
